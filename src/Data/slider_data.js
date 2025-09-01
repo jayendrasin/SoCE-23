19ce7274e32521fd331ecff23301b096ecbf640e
jayendrasin <jayendrasingh027@gmail.com>
2025-09-01 21:39:48 +0530

    slider  updated

--- src/Data/slider_data.js
+++ src/Data/slider_data.js
@@ -1,4 +1,11 @@
 const content = [
+  
+  [
+    "On August 12, 2025 We conducted an informal session for Y24s, organized by Y23s, on the theme “How to Master the 3rd Semester.” In the session, we shared our personal experiences, strategies, and challenges faced during the semester. We discussed effective time management, balancing academics with extracurriculars, and tips for excelling in core courses. The session also highlighted the importance of consistency, resource-sharing, and peer learning. It was an interactive and insightful discussion aimed at guiding juniors for a smoother journey ahead.",
+    "Informal Session – for Y24s",
+    "August 12, 2025",
+    "/info.pdf"
+  ],
   [
     "The Alumni Festival – PARWA (Postgraduate Alumni Reconnecting With Alma Mater) will be held on September 6, 2025, from 2:30 P.M. to 4:30 P.M. at FB 320.This event will bring our alumni back to campus to share their journeys and insights. It will give students a great opportunity to learn, network, and explore career opportunities. Alumni will share their valuable experiences, and participants will be able to engage in meaningful discussions.PARWA 2025 will strengthen the bond between our alumni and the institute, and we are looking forward to enthusiastic participation",
     "Alumni Festival – PARWA 2025",
@@ -12,11 +19,7 @@ const content = [
     "",
     "https://drive.google.com/drive/folders/1mRabKkE9Wu8sk_P15ioLKm7D5ohstrxV",
   ],
-  [
-    "",
-    "",
-    "",
-  ],
+  
   [
     "",
     "",
@@ -26,9 +29,9 @@ const content = [
 ];
 
 const img = [
-  "",
-  "",
   "Post-photos/informal.jpeg",
+  "",
+  "",
   "",
 ];
 
